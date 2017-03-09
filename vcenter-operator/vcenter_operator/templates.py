8f2e454a23a4206e3d69527048c66612c7b603c5
Fabian Wiesel <fabian.wiesel@sap.com>
2017-03-09 09:33:59 +0100

    Move quoting to own filter function

--- vcenter-operator/vcenter_operator/templates.py
+++ vcenter-operator/vcenter_operator/templates.py
@@ -1,5 +1,12 @@
 from jinja2 import ChoiceLoader, Environment, FileSystemLoader, PackageLoader
 
+
+def _quote(value):
+    return '"{}"'.format(_ini_escape(value).replace('"', '\\"'))
+
+def _ini_escape(value):
+    return str(value).replace('$', '$$')
+
 env = Environment(loader=ChoiceLoader([FileSystemLoader('/var/lib/kolla/config_files', followlinks=True), PackageLoader('vcenter_operator', 'templates')]))
-
-
+env.filters['ini_escape'] = _ini_escape
+env.filters['quote'] = _quote
