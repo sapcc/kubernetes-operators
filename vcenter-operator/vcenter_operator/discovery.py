0a973b4840ced1f869b28838f3f7d5e6b664ef0a
Fabian Wiesel <fabian.wiesel@sap.com>
2017-04-03 11:28:02 +0200

    [vcenter-operator] Option for specifying discovery-dns ip & port

--- vcenter-operator/vcenter_operator/discovery.py
+++ vcenter-operator/vcenter_operator/discovery.py
@@ -26,13 +26,15 @@ class DnsDiscovery(object):
         self.namespace = global_options['namespace']
         token = client.configuration.auth_settings().get('BearerToken', None)
         self.cluster_internal = token and token['type'] == 'api_key' and not not token['value']
-        self.ip = None
-        self._init_dns()
+        self.ip = global_options.get('dns_ip', None)
+        self.port = global_options.get('dns_port', 53)
+        if not self.ip:
+            self._discover_dns()
 
     def register(self, pattern, callback):
         self._patterns[pattern].callbacks.append(callback)
 
-    def _init_dns(self):
+    def _discover_dns(self):
         for item in client.CoreV1Api().list_namespaced_service(namespace=self.namespace,
                                                                label_selector='component=designate,type=backend').items:
             spec = item.spec
