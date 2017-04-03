8b790e44c55fb8662b2befbd5fefde695672c00c
Rudolf Vriend <rudolf.vriend@sap.com>
2017-04-03 12:31:58 +0200

    openstack-operagtor: log namespace of seed specs

--- openstack-operator/python/setup.py
+++ openstack-operator/python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='openstack_seeder',
-    version='0.0.2',
+    version='0.0.3',
     packages='.',
     install_requires=[
         'requests==2.12.5',
