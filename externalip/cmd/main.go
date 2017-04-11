40658907fef42653e8a9829c01d297e392e99914
Fabian Ruff <fabian.ruff@sap.com>
2017-04-11 17:04:51 +0200

    Initial version of external-ip operator

---
+++ externalip/cmd/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"flag"
+	"net"
+	"os"
+	"os/signal"
+	"strings"
+	"sync"
+	"syscall"
+
+	"github.com/golang/glog"
+	"github.com/sapcc/kubernetes-operators/externalip/pkg/operator"
+	"github.com/spf13/pflag"
+)
+
+var (
+	options         operator.Options
+	ignoreAddresses []string
+)
+
+func init() {
+	pflag.StringVar(&options.KubeConfig, "kubeconfig", "", "Path to kubeconfig file with authorization and master location information.")
+	pflag.StringVar(&options.NetworkInterface, "interface", "", "Network interface where to attach external ips")
+	pflag.StringSliceVar(&ignoreAddresses, "ignore-address", []string{}, "Don't remove or add specified ip address")
+
+}
+
+func main() {
+
+	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
+	pflag.Parse()
+
+	if _, err := net.InterfaceByName(options.NetworkInterface); err != nil {
+		glog.Fatal("No network interface specified or the interface doesn't exist")
+	}
+	for _, address := range ignoreAddresses {
+		var t = address
+		if !strings.Contains(address, "/") {
+			t = address + "/32"
+		}
+		_, cidr, err := net.ParseCIDR(t)
+		if err != nil {
+			glog.Fatalf("Invalid IP or CIDR Range: %s", address, err)
+		}
+		options.IgnoreCIDR = append(options.IgnoreCIDR, *cidr)
+	}
+
+	sigs := make(chan os.Signal, 1)
+	stop := make(chan struct{})
+	signal.Notify(sigs, os.Interrupt, syscall.SIGTERM) // Push signals into channel
+
+	wg := &sync.WaitGroup{} // Goroutines can add themselves to this to be waited on
+
+	go operator.New(options).Run(1, stop, wg)
+
+	<-sigs // Wait for signals (this hangs until a signal arrives)
+	glog.Infof("Shutting down...")
+
+	close(stop) // Tell goroutines to stop themselves
+	wg.Wait()   // Wait for all to be stopped
+}
