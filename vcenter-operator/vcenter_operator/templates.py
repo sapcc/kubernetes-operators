4ab4a90ef91898404cddb7f0eabeaae348775d79
Fabian Wiesel <fabian.wiesel@sap.com>
2017-12-07 11:10:40 +0100

    [vcenter-operator] render and shasum filter for checksum annotations

--- vcenter-operator/vcenter_operator/templates.py
+++ vcenter-operator/vcenter_operator/templates.py
@@ -1,6 +1,7 @@
 import logging
+import hashlib
 
-from jinja2 import ChoiceLoader, Environment, FileSystemLoader, PackageLoader, contextfilter
+from jinja2 import ChoiceLoader, Environment, FileSystemLoader, PackageLoader, contextfilter, environmentfilter
 
 from masterpassword import MasterPassword
 
@@ -22,8 +23,22 @@ def _derive_password(ctx, username=None):
     return mpw.derive('long', ctx['host']).replace("/", "")
 
 
+def _sha256sum(data):
+    sha1 = hashlib.new('sha256')
+    sha1.update(data)
+    return sha1.hexdigest()
+
+
+@contextfilter
+def _render(ctx, template_name):
+    template = ctx.environment.get_template(template_name)
+    return template.render(ctx)
+
+
 env = Environment(loader=ChoiceLoader([FileSystemLoader('/var/lib/kolla/config_files', followlinks=True),
                                        PackageLoader('vcenter_operator', 'templates')]))
 env.filters['ini_escape'] = _ini_escape
 env.filters['quote'] = _quote
 env.filters['derive_password'] = _derive_password
+env.filters['sha256sum'] = _sha256sum
+env.filters['render'] = _render
