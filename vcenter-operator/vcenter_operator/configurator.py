a34c2dc17ffc398555298140cd68124716ac5b52
Fabian Wiesel <fabian.wiesel@sap.com>
2017-12-27 14:27:23 +0100

    [vcenter-operator] Destroy and create object view
    This hopefully should close leaking view references

--- vcenter-operator/vcenter_operator/configurator.py
+++ vcenter-operator/vcenter_operator/configurator.py
@@ -4,6 +4,7 @@ import re
 import ssl
 import sys
 from collections import defaultdict, deque
+from contextlib import contextmanager
 from os.path import commonprefix
 from socket import error as socket_error
 
@@ -17,7 +18,19 @@ from .phelm import DeploymentState
 from .templates import env
 from .vcenter_util import *
 
-log = logging.getLogger(__name__)
+LOG = logging.getLogger(__name__)
+
+
+@contextmanager
+def filter_spec_context(service_instance):
+    try:
+        obj_type = vim.ClusterComputeResource
+        view_ref = get_container_view(service_instance, obj_type=[obj_type])
+        yield create_filter_spec(view_ref=view_ref,
+                                 obj_type=obj_type,
+                                 path_set=['name', 'parent', 'datastore', 'network'])
+    finally:
+        view_ref.DestroyView()
 
 
 class Configurator(object):
@@ -44,7 +57,7 @@ class Configurator(object):
 
                 password = self.mpw.derive('long', host).replace("/", "")  # Vcenter doesn't accept / in password
 
-                log.info("{}".format(host))
+                LOG.info("{}".format(host))
                 if hasattr(ssl, '_create_unverified_context'):
                     context = ssl._create_unverified_context()
 
@@ -57,77 +70,70 @@ class Configurator(object):
                 if service_instance:
                     atexit.register(Disconnect, service_instance)
 
-                    obj_type = vim.ClusterComputeResource
-                    view_ref = get_container_view(service_instance, obj_type=[obj_type])
-                    filter_spec = create_filter_spec(view_ref=view_ref,
-                                                     obj_type=obj_type,
-                                                     path_set=['name', 'parent', 'datastore', 'network'])
-
                     self.vcenters[host] = {'service_instance': service_instance,
                                            'username': self.username,
                                            'password': password,
                                            'host': host,
                                            'name': name,
-                                           'filter_spec': filter_spec
                                            }
 
             except vim.fault.InvalidLogin as e:
-                log.error("%s: %s", host, e.msg)
+                LOG.error("%s: %s", host, e.msg)
             except socket_error as e:
-                log.error("%s: %s", host, e)
+                LOG.error("%s: %s", host, e)
 
         if removed:
-            log.info("Gone vcs {}".format(removed))
+            LOG.info("Gone vcs {}".format(removed))
 
     def _poll(self, host):
         vcenter_options = self.vcenters[host]
         service_instance = vcenter_options['service_instance']
-        filter_spec = vcenter_options['filter_spec']
+        with filter_spec_context(service_instance) as filter_spec:
+            availability_zones = set()
 
-        availability_zones = set()
+            for cluster in collect_properties(service_instance, [filter_spec]):
+                cluster_name = cluster['name']
+                match = self.CLUSTER_MATCH.match(cluster_name)
 
-        for cluster in collect_properties(service_instance, [filter_spec]):
-            cluster_name = cluster['name']
-            match = self.CLUSTER_MATCH.match(cluster_name)
+                if not match:
+                    LOG.warning("%s: Ignoring cluster %s", host, cluster_name)
+                    continue
 
-            if not match:
-                log.warning("%s: Ignoring cluster %s", host, cluster_name)
-                continue
+                parent = cluster['parent']
+                availability_zone = parent.parent.name.lower()
+                availability_zones.add(availability_zone)
+                cluster_options = self.global_options.copy()
+                cluster_options.update(vcenter_options)
+                cluster_options.update(name=match.group(1).lower(),
+                                       cluster_name=cluster_name,
+                                       availability_zone=availability_zone)
 
-            parent = cluster['parent']
-            availability_zone = parent.parent.name.lower()
-            availability_zones.add(availability_zone)
-            cluster_options = self.global_options.copy()
-            cluster_options.update(vcenter_options)
-            cluster_options.update(name=match.group(1).lower(),
-                                   cluster_name=cluster_name,
-                                   availability_zone=availability_zone)
+                if cluster_options.get('pbm_enabled', 'false') != 'true':
+                    datastores = cluster['datastore']
+                    datastore_names = [datastore.name for datastore in datastores if
+                                       self.EPH_MATCH.match(datastore.name)]
+                    eph = commonprefix(datastore_names)
+                    cluster_options.update(datastore_regex="^{}.*".format(eph))
 
-            if cluster_options.get('pbm_enabled', 'false') != 'true':
-                datastores = cluster['datastore']
-                datastore_names = [datastore.name for datastore in datastores if self.EPH_MATCH.match(datastore.name)]
-                eph = commonprefix(datastore_names)
-                cluster_options.update(datastore_regex="^{}.*".format(eph))
+                for network in cluster['network']:
+                    match = self.BR_MATCH.match(network.name)
+                    if match:
+                        cluster_options['bridge'] = match.group(0).lower()
+                        cluster_options['physical'] = match.group(1).lower()
+                        break
 
-            for network in cluster['network']:
-                match = self.BR_MATCH.match(network.name)
-                if match:
-                    cluster_options['bridge'] = match.group(0).lower()
-                    cluster_options['physical'] = match.group(1).lower()
-                    break
+                cluster_state = self.clusters[cluster_name]
+                config_hash = hash(frozenset(cluster_options.items()))
+                cluster_options['config_hash'] = config_hash + sys.maxsize
+                cluster_state['config_hash'] = config_hash
+                self._add_code('vcenter_cluster', cluster_options)
 
-            cluster_state = self.clusters[cluster_name]
-            config_hash = hash(frozenset(cluster_options.items()))
-            cluster_options['config_hash'] = config_hash + sys.maxsize
-            cluster_state['config_hash'] = config_hash
-            self._add_code('vcenter_cluster', cluster_options)
+            for availability_zone in availability_zones:
+                cluster_options = self.global_options.copy()
+                cluster_options.update(vcenter_options)
+                cluster_options.update(availability_zone=availability_zone)
 
-        for availability_zone in availability_zones:
-            cluster_options = self.global_options.copy()
-            cluster_options.update(vcenter_options)
-            cluster_options.update(availability_zone=availability_zone)
-
-        self._add_code('vcenter_datacenter', cluster_options)
+            self._add_code('vcenter_datacenter', cluster_options)
 
     def _add_code(self, scope, options):
         for template_name in env.list_templates(filter_func=lambda x: x.startswith(scope) and x.endswith('.yaml.j2')):
@@ -168,7 +174,7 @@ class Configurator(object):
             try:
                 self._poll(host)
             except six.moves.http_client.HTTPException as e:
-                log.warning("%s: %r", host, e)
+                LOG.warning("%s: %r", host, e)
                 continue
 
         if len(self.states) > 1:
