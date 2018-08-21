c02db20e36a7e13dcbb0613c93a6234bf9c1ffe1
Rudolf Vriend <rudolf.vriend@sap.com>
2018-08-21 13:30:50 +0200

    openstack-seeder: updated client dependencies, corrected error message typos

--- openstack-seeder/python/setup.py
+++ openstack-seeder/python/setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name='openstack_seeder',
-    version='0.1.1',
+    version='0.1.2',
     packages='.',
     install_requires=[
-        'python-keystoneclient==3.15.0',
-        'python-novaclient==10.1.0',
-        'python-neutronclient==6.7.0',
-        'python-designateclient==2.9.0',
-        'python-swiftclient==3.5.0',
+        'python-keystoneclient==3.17.0',
+        'python-novaclient==11.0.0',
+        'python-neutronclient==6.9.0',
+        'python-designateclient==2.10.0',
+        'python-swiftclient==3.6.0',
         'PyYAML==3.12',
         'raven',
     ],
