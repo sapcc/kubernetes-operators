fc58dc203daa6c44c34205658412dc835278b0a3
Rudolf Vriend <rudolf.vriend@sap.com>
2019-01-17 10:31:10 +0100

    [openstack-seeder] python dependency update

--- openstack-seeder/python/setup.py
+++ openstack-seeder/python/setup.py
@@ -2,17 +2,17 @@ from setuptools import setup
 
 setup(
     name='openstack_seeder',
-    version='0.1.4',
+    version='0.1.5',
     packages='.',
     install_requires=[
-        'python-keystoneclient==3.17.0',
-        'python-novaclient==11.0.0',
-        'python-neutronclient==6.10.0',
-        'python-designateclient==2.10.0',
+        'python-keystoneclient==3.18.0',
+        'python-novaclient==11.1.0',
+        'python-neutronclient==6.11.0',
+        'python-designateclient==2.11.0',
         'python-swiftclient==3.6.0',
         'osc-placement==1.3.0',
-        'PyYAML==3.12',
         'raven',
+        'pyyaml>=4.2b4',
     ],
     url='https://github.com/sapcc/kubernetes-operators/openstack-seeder',
     license='',
