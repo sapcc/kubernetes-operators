8b99c11d68186e4b1c5ea707e42b6a820cf2a3bc
Rudolf Vriend <rudolf.vriend@sap.com>
2019-04-24 14:35:35 +0200

    updated k8s 1.10 dependencies

--- openstack-seeder/hack/deps.go
+++ openstack-seeder/hack/deps.go
@@ -10,4 +10,5 @@ import (
 	_ "k8s.io/code-generator/cmd/deepcopy-gen"
 	_ "k8s.io/code-generator/cmd/informer-gen"
 	_ "k8s.io/code-generator/cmd/lister-gen"
+	_ "k8s.io/code-generator/cmd/openapi-gen"
 )
