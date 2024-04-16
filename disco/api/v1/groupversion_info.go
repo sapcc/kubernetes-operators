ef11816f3d99f8e739a90d07a79cd5572e9c60e8
Jan Knipper <jan.knipper@sap.com>
2024-04-16 15:50:25 +0200

    Bump disco operator dependencies

--- disco/api/v1/groupversion_info.go
+++ disco/api/v1/groupversion_info.go
@@ -15,8 +15,8 @@ limitations under the License.
 */
 
 // Package v1 contains API Schema definitions for the disco v1 API group
-//+kubebuilder:object:generate=true
-//+groupName=disco.stable.sap.cc
+// +kubebuilder:object:generate=true
+// +groupName=disco.stable.sap.cc
 package v1
 
 import (
--- disco/main.go
+++ disco/main.go
@@ -30,6 +30,8 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"sigs.k8s.io/controller-runtime/pkg/metrics/server"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
 
 	discov1 "github.com/sapcc/kubernetes-operators/disco/api/v1"
 	"github.com/sapcc/kubernetes-operators/disco/controllers"
@@ -96,9 +98,11 @@ func main() {
 	}
 
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-		Scheme:                  scheme,
-		MetricsBindAddress:      metricsAddr,
-		Port:                    9443,
+		Scheme: scheme,
+		Metrics: server.Options{
+			BindAddress: metricsAddr,
+		},
+		WebhookServer:           webhook.NewServer(webhook.Options{Port: 9443}),
 		HealthProbeBindAddress:  probeAddr,
 		LeaderElection:          true,
 		LeaderElectionID:        "disco.controller",
